c7d264b0a97093d506295413e93d25eff83a9613
Olivier Lamontagne <olamontagne@edu.cegepgarneau.ca>
2021-01-31 17:17:48 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+Banque_Mot = {
+    "manette": "Appareil utilisé pour jouer à certains jeux vidéo",
+    "bouteille": "Conteneur de plastique conçue pour tenir un liquide",
+    "bureau": "Surface de travail",
+    ""
+}
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
